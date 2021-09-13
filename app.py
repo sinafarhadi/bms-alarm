5d1f24be0bfd6460df0807e88c5e250b611e4ed2
Sina <sina.farhadi@gmail.com>
2021-09-13 17:17:23 +0430

    send and receive ok

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import requests
+import config
+
 app = Flask(__name__)
 
 
@@ -7,3 +10,41 @@ def main_page():
     '''This is the mail page og the site
     '''
     return 'Hello'
+
+
+@app.route('/v1/proccess', methods=['POST'])
+def proccess():
+    """
+    This is a call back from kavehnegar
+    """
+
+    data = request.form
+    sender = data["from"]
+    message = data["message"]
+    print (f'received {message} from {sender}')
+
+    ret = {"message": "processed"}
+
+    send_sms({sender}, f'hi {sender}')
+
+    return jsonify(ret), 200
+
+def send_sms(receptor, message):
+    url = f'https://api.kavenegar.com/v1/{config.kavenegar_API}/sms/send.json'
+    data = {"sender": {config.kavenegar_number},
+            "receptor": receptor,
+            "message": message,
+            
+            }
+
+    res = requests.post(url, data)
+    print(f"message {message} sent to {receptor}. status code is {res.status_code}")
+
+def check_serial():
+    pass
+
+
+
+if __name__ == "__main__":
+    
+    app.run("0.0.0.0", 5000)
